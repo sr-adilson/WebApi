01c50e0707fbe79b20e11ce4d02c5f87bf761b53
Adilson <sr.adilson@gmail.com>
2021-08-20 09:10:30 -0300

    Create Data

---
+++ WebApi/SiteApi/Context/MusicaContext.cs
@@ -0,0 +1,18 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class MusicaContext : DbContext
+    {
+        public DbSet<Musica> Musica { get; set; }
+        public MusicaContext() : base("Data Source=192.168.0.192;Initial Catalog=Adilson;Persist Security Info=True;User ID=Adilson;Password=123")
+        {
+        }
+    }
+}
